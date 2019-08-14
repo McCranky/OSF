94fec358eb1ba4abf28c7279b427571dde44fe2d
Marek Smatana <smatana13@stud.uniza.sk>
2019-08-14 08:26:02 +0200

    Added categories

---
+++ UC_Oddelenia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSF {
+    public partial class UC_Oddelenia : UserControl {
+        public UC_Oddelenia() {
+            InitializeComponent();
+        }
+    }
+}
